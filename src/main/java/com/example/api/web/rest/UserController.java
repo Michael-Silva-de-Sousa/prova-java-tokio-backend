bbac00be3e38eb775496129990a6bab08e93f70b
michael <michael.sp.contato@gmail.com>
2020-06-02 02:11:22 -0300

    Documentacao com Swagger da Api UserController.

--- src/main/java/com/example/api/web/rest/UserController.java
+++ src/main/java/com/example/api/web/rest/UserController.java
@@ -5,21 +5,21 @@ import com.example.api.dto.TokenDTO;
 import com.example.api.exceptions.PasswordInvalidException;
 import com.example.api.security.jwt.JwtService;
 import com.example.api.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
+@Api("Api Users")
 public class UserController {
 
     @Autowired
@@ -29,6 +29,12 @@ public class UserController {
     public JwtService jwtService;
 
     @PostMapping("/auth")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Permite autenticar o usuário na aplicação")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Token válido. Usuario autenticado com sucesso"),
+            @ApiResponse(code = 401, message = "Token inválido. Credenciais inválidas")
+    })
     public TokenDTO authenticate(@RequestBody CredentialDTO credentialDTO){
         try{
             UserDetails userAuth = userService.authenticate(
